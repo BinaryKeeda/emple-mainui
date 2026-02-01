41da0cc0238bfdd433f70b237745f56eac75983d
Aryan <aryan@rival.io>
2026-02-01 14:19:53 +0530

    Build Issure fixed

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,18 @@ import { Provider } from 'react-redux'
 import store from './redux/store.js'
 
 import { ApolloProvider } from '@apollo/client'
-import { client } from './lib/config.js'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from '@descope/react-sdk'
 import { UserProvider } from './context/UserContext'
 import { HelmetProvider } from 'react-helmet-async'
-import { Suspense } from 'react'
-import Loader from './view/test/components/Loader'
 import Application from './Application'
+import { client } from './lib/Lib'
 
 const queryClient = new QueryClient()
 const isProd = import.meta.env.MODE === 'production'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <AuthProvider
     projectId={import.meta.env.VITE_APP_DESCOPE_PROJECT_ID}
     sessionTokenViaCookie={isProd}
